5f87817e42cf1af9d4c42710a0e0c4e2bfe43010
Fermin Villanueva <FerVillanuev4s@gmail.com>
2020-11-19 15:21:18 -0700

    sanctum

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('/sanctum/token', [App\Http\Controllers\Api\SanctumController::class, 'token']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
